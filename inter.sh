e8d342a5e1a4998d93ae010312c51261f4ef7928
Liam Laidlaw <liamlaidlaw04@gmail.com>
2025-07-18 23:41:39 -0600

    added laoding

--- inter.sh
+++ inter.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+module purge
+module load slurm
+module load cudnn8.5-cuda11.7/8.5.0.96
+module load gcc/10.2.0
+echo "Modules loaded."
+
+mamba activate FederatedResnet
+echo "Activated Conda environment: $CONDA_DEFAULT_ENV"
 srun --nodes=1 --partition=gpu-l40 --gpus-per-node=4 --ntasks-per-node=1 --cpus-per-task=12 --pty bash -i
