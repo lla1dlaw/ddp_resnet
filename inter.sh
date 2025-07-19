6c4994f226443d5d3612d5677de2a0522a04cec6
Liam Laidlaw <liamlaidlaw04@gmail.com>
2025-07-18 23:39:01 -0600

    sync

--- inter.sh
+++ inter.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-srun --nodes=1 --partition=gpu-l40 --gpus-per-node=4 --ntasks-per-node=4 --cpus-per-task=12 --pty bash -i
+srun --nodes=1 --partition=gpu-l40 --gpus-per-node=4 --ntasks-per-node=1 --cpus-per-task=12 --pty bash -i
