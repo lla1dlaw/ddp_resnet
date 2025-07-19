30ca69b4968d969950518a69c9b810836728d870
Liam Laidlaw <liamlaidlaw04@gmail.com>
2025-07-18 23:47:40 -0600

    added loading

--- inter.sh
+++ inter.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
+srun --nodes=1 --partition=gpu-l40 --gpus-per-node=4 --ntasks-per-node=1 --cpus-per-task=12 --pty bash -i
+
 module purge
-module load slurm
 module load cudnn8.5-cuda11.7/8.5.0.96
-module load gcc/10.2.0
 echo "Modules loaded."
 
-mamba activate FederatedResnet
+conda activate FederatedResnet
 echo "Activated Conda environment: $CONDA_DEFAULT_ENV"
-srun --nodes=1 --partition=gpu-l40 --gpus-per-node=4 --ntasks-per-node=1 --cpus-per-task=12 --pty bash -i
+
+# --- Set up environment for torchrun ---
+export MASTER_ADDR=$(scontrol show hostnames $SLURM_JOB_NODELIST | head -n 1)
+export MASTER_PORT=12355 # A static port is fine for single-node jobs
+
+torchrun \
+  --standalone \
+  --nproc_per_node=$SLURM_TASKS_PER_NODE \
+  --nnodes=$SLURM_NNODES \
+  train.py --epochs 200 --batch_size 64 --dataset cifar10 --model-type real
