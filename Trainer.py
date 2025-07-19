fa313012e340bb1b5a2b99416be5c3da865179be
Liam Laidlaw <liamlaidlaw04@gmail.com>
2025-07-18 19:22:17 -0600

    fixed script and added time logging

--- Trainer.py
+++ Trainer.py
@@ -1,5 +1,5 @@
 """An abstracted trainer that wraps a model in DDP and handles training."""
-
+from datetime import datetime
 import torch
 import torch.nn.functional as F
 from torch.utils.data import DataLoader
@@ -51,6 +51,7 @@ class Trainer:
         self.train_data.sampler.set_epoch(epoch)
 
         self.model.train()
+        epoch_start = datetime.now()
         for inputs, targets in self.train_data:
             inputs = inputs.to(self.gpu_id)
             targets = targets.to(self.gpu_id)
@@ -59,11 +60,13 @@ class Trainer:
             top1_acc.update(outputs, targets)
             top5_acc.update(outputs, targets)
             progress_bar.update(task_id, description=f"Epoch {epoch+1} ", advance=1)
-
+        epoch_end = datetime.now()
+        total_epoch_duration = epoch_end - epoch_start
+        epoch_duration_seconds = total_epoch_duration.total_seconds()
         epoch_loss = loss_total / len(self.train_data)
-        return epoch_loss, top1_acc.compute().item(), top5_acc.compute().item()
+        return epoch_loss, top1_acc.compute().item(), top5_acc.compute().item(), epoch_duration_seconds
 
-    def validate(self, epoch: int):
+    def validate(self):
         top1_acc = MulticlassAccuracy(self.num_classes, top_k=1)
         top5_acc = MulticlassAccuracy(self.num_classes, top_k=5)
         total_loss = 0
@@ -103,8 +106,8 @@ class Trainer:
         with Progress() as progress_bar:
             task = progress_bar.add_task(description="Epoch 1 ", total=total_steps)
             for epoch in range(max_epochs):
-                epoch_loss, train_top1, train_top5 = self._run_epoch(epoch, progress_bar, task)
-                val_loss, val_top1, val_top5 = self.validate(epoch)
+                epoch_loss, train_top1, train_top5, epoch_duration = self._run_epoch(epoch, progress_bar, task)
+                val_loss, val_top1, val_top5 = self.validate()
                 run.log({
                     "train loss": epoch_loss,
                     "train acc": train_top1,
@@ -112,9 +115,9 @@ class Trainer:
                     "val loss": val_loss,
                     "val acc": val_top1,
                     "val top5 acc": val_top5,
+                    "epoch_duration_sec": epoch_duration,
                 })
                 if self.gpu_id == 0 and epoch % self.save_every == 0:
                     self._save_checkpoint(epoch)
-                self.validate(epoch)
         run.finish()
 
