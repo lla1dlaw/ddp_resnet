ed80a24694b53e750a75180c379afe715fa2af6e
Liam Laidlaw <liamlaidlaw04@gmail.com>
2025-07-18 20:06:06 -0600

    typo

--- distributed_data_parallel_slurm_run.bash
+++ distributed_data_parallel_slurm_run.bash
@@ -7,4 +7,3 @@ eval $setup_str
 # TODO: You can simply call your DDP-enabled script here. See example.py for an example.
 # Docs: https://pytorch.org/docs/stable/generated/torch.nn.parallel.DistributedDataParallel.html
 python train.py
-
