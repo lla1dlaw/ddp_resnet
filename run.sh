e8eb742a91bafd4b009dbcb79c6a5af072a84265
Liam Laidlaw <liamlaidlaw04@gmail.com>
2025-07-18 21:17:24 -0600

    logs fixed

--- run.sh
+++ run.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 
 cd ./logs/
-FILE_TO_WAIT_FOR="output.log"
+FILE_TO_WAIT_FOR='output.log'
 TIMEOUT_SECONDS=60 # Optional: set a timeout
 
 rm -r ./*
+cd ..
 sbatch run.sbatch
 echo "Waiting for $FILE_TO_WAIT_FOR to be created..."
 
 # Loop until the file exists or timeout is reached
 start_time=$(date +%s)
-until [ -f "$FILE_TO_WAIT_FOR" ] || (($(date +%s) - start_time >= TIMEOUT_SECONDS)); do
+until [ -f "./logs/$FILE_TO_WAIT_FOR" ] || (($(date +%s) - start_time >= TIMEOUT_SECONDS)); do
   sleep 1 # Check every second
 done
 
-if [ -f "$FILE_TO_WAIT_FOR" ]; then
-  echo "$FILE_TO_WAIT_FOR has been created."
-  tail -f -n 100 $FILE_TO_WAIT_FOR
+if [ -f "./logs/$FILE_TO_WAIT_FOR" ]; then
+  echo "./logs/$FILE_TO_WAIT_FOR has been created."
+  tail -f -n 100 ./logs/$FILE_TO_WAIT_FOR
 else
-  echo "Timeout: $FILE_TO_WAIT_FOR was not created within $TIMEOUT_SECONDS seconds."
+  echo "Timeout: ./logs/$FILE_TO_WAIT_FOR was not created within $TIMEOUT_SECONDS seconds."
   exit 1 # Exit with an error code
 fi
