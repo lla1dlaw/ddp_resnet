6c334f1cb44f69675bb69a8f3dd287e763645458
Liam Laidlaw <liamlaidlaw04@gmail.com>
2025-07-20 16:06:16 -0600

    Changed git ignore and launch script

--- launch.sh
+++ launch.sh
@@ -22,9 +22,9 @@ echo "--- 🚀 Launching training script via torchrun... ---"
 torchrun \
   --standalone \
   --nproc_per_node=$SLURM_GPUS_PER_NODE \
-  ./train.py --epochs 200 --batch_size 64 --dataset S1SLC_CVDL --trials 5 --model-type real
+  ./train.py --epochs 200 --batch_size 64 --dataset S1SLC_CVDL_HH --trials 5 --model-type real
 
 torchrun \
   --standalone \
   --nproc_per_node=$SLURM_GPUS_PER_NODE \
-  ./train.py --epochs 200 --batch_size 64 --dataset S1SLC_CVDL --trials 5 --model-type complex
+  ./train.py --epochs 200 --batch_size 64 --dataset S1SLC_CVDL_HH --trials 5 --model-type complex
