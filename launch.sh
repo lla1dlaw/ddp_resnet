d46c871915ca34edeef2374095b2871c98fcde17
Liam Laidlaw <liamlaidlaw04@gmail.com>
2025-07-20 18:09:32 -0600

    set threads to 8

--- launch.sh
+++ launch.sh
@@ -20,12 +20,12 @@ echo "Activated Conda environment: $CONDA_DEFAULT_ENV"
 # torchrun will spawn a process for each GPU and set LOCAL_RANK correctly.
 echo "--- Launching training script via torchrun ---"
 
-torchrun \
+OMP_NUM_THREADS=8 torchrun \
   --standalone \
   --nproc_per_node=$SLURM_GPUS_PER_NODE \
   ./train.py --epochs 200 --batch_size 64 --dataset S1SLC_CVDL_HH --trials 5 --model-type real
 
-torchrun \
+OMP_NUM_THREADS=8 torchrun \
   --standalone \
   --nproc_per_node=$SLURM_GPUS_PER_NODE \
   ./train.py --epochs 200 --batch_size 64 --dataset S1SLC_CVDL_HH --trials 5 --model-type complex
