8189d00405a349458db985dfd2495e87bb3d3a0c
Liam Laidlaw <liamlaidlaw04@gmail.com>
2025-07-20 19:27:48 -0600

    re loading dot env

--- Trainer.py
+++ Trainer.py
@@ -136,6 +136,7 @@ class Trainer:
 
 
     def send_wand_link(self, url: str):
+        load_dotenv()
         email_user = os.getenv("EMAIL_USER")
         email_pass = os.getenv("EMAIL_PASS")
         if not email_user or not email_pass:
